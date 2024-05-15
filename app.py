74a68e17d1531b0c439ad416686a81bfc187c342
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-05-15 15:47:23 -0400

    list and its methods/functions

--- app.py
+++ app.py
@@ -31,10 +31,38 @@ print(sqrt(144))
 # print(result)
 
 # game
-color = input("your color: ")
-plural_noun = input("your plural noun: ")
-celeb = input("your fav celebrity: ")
-print(f"Roses are {color}")
-print(f"{plural_noun} are blue")
-print(f"I love {celeb}")
+# color = input("your color: ")
+# plural_noun = input("your plural noun: ")
+# celeb = input("your fav celebrity: ")
+# print(f"Roses are {color}")
+# print(f"{plural_noun} are blue")
+# print(f"I love {celeb}")
+
+# lists
+numbers = [1, 2, 3, 45,2,  56, 2]
+colors = ["red", "blue", "grey", "green"]
+
+# print(colors[1:-1])
+# print(len(colors))
+colors.extend(numbers)
+# add an item to the end of  the list
+colors.append("white")
+# add to the specific place/position into the list
+colors.insert(2, "yellow")
+# remove an item
+colors.remove("red")
+# remove all the elements
+# colors.clear()
+# remove the last element off the list
+colors.pop()
+# check and get the index of an element
+print(colors.index('grey'))
+print(colors.count(2))
+numbers.sort()
+numbers.reverse()
+print(numbers)
+colors2 = colors.copy()
+print(colors2)
+print(colors)
+
 
