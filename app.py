4303ef4b29138330d79a5dfc456a0cadbc37f0d2
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-05-15 10:59:29 -0400

    Numbers in Python

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+
+from math import *
+# 'math' - module, where we get functions
+# strings
 phrase = 'Hello World'
 print(phrase.lower())
 print(phrase.upper())
@@ -7,5 +11,12 @@ print(len(phrase))
 print(phrase[-1])
 print(phrase.lower().index("w"))
 print(phrase.replace("World", "Ann"))
+
+# numbers
+my_num = 27
+print(str(my_num) + " my fav number")
+print(floor(3.2))
+print(ceil(3.2))
+print(sqrt(144))
 
 
