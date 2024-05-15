0b4d603fdcbd8b0ec559cba707c683f623a0a8ce
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-05-15 10:47:31 -0400

    Strings in Python

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-print('Hello World')
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+phrase = 'Hello World'
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[-1])
+print(phrase.lower().index("w"))
+print(phrase.replace("World", "Ann"))
+
+
